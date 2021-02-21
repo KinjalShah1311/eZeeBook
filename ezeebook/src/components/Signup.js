9ccf0b017a03cfb7c510656d8f450804d2739a81
Kinjal Shah <kinjal_shah1311@yahoo.com>
2021-02-21 18:52:01 -0500

    signup and login added with route to dashboard

--- ezeebook/src/components/Signup.js
+++ ezeebook/src/components/Signup.js
@@ -3,23 +3,24 @@ import Avatar from "@material-ui/core/Avatar";
 import Button from "@material-ui/core/Button";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import TextField from "@material-ui/core/TextField";
-import FormControlLabel from "@material-ui/core/FormControlLabel";
-import Checkbox from "@material-ui/core/Checkbox";
-import Link from "@material-ui/core/Link";
+import Alert from '@material-ui/lab/Alert';
 import Grid from "@material-ui/core/Grid";
 import Box from "@material-ui/core/Box";
-//import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
+import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
 import Typography from "@material-ui/core/Typography";
 import { makeStyles } from "@material-ui/core/styles";
 import Container from "@material-ui/core/Container";
+
+import { Link, useHistory } from "react-router-dom";
+
 import { useAuth } from "../contexts/AuthContext";
 
 function Copyright() {
   return (
     <Typography variant="body2" color="textSecondary" align="center">
       {"Copyright © "}
-      <Link color="inherit" href="https://material-ui.com/">
-        Your Website
+      <Link color="inherit" href="#">
+        eZeeBook
       </Link>{" "}
       {new Date().getFullYear()}
       {"."}
@@ -47,39 +48,50 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 export default function Signup() {
-  const fNameRef = useRef();
-  const lNameRef = useRef();
+  // const fNameRef = useRef();
+  // const lNameRef = useRef();
   const emailRef = useRef();
   const passwordRef = useRef();
-  const { signup } = useAuth();
-  const [error, setError] = useState('');
+  const cofirmpasswordRef = useRef();
+  const history = useHistory();
+
+  const { signup, currentUser } = useAuth();
+  const [error, setError] = useState("");
   const [loading, setLoading] = useState(false);
 
   const classes = useStyles();
 
   function handleSubmit(e) {
-      e.preventDefault();
-      try{
-          setError('')
-          setLoading(true)
-          signup(emailRef.current.value, passwordRef.current.value)
-      } catch {
-        setError('Failed to create an account')
-      }
-      setLoading(false);
+    e.preventDefault();
+    if (passwordRef.current.value !== cofirmpasswordRef.current.value) {
+      setError("Passwords do not match");
+      return;
+    }
+    try {
+      setError("");
+      setLoading(true);
+      signup(emailRef.current.value, passwordRef.current.value);
+      history.push("/");
+    } catch {
+      setError("Failed to create an account");
+    }
+
+    setLoading(false);
   }
 
   return (
     <Container component="main" maxWidth="xs">
       <CssBaseline />
       <div className={classes.paper}>
-        <Avatar className={classes.avatar}>{/* <LockOutlinedIcon /> */}</Avatar>
+        <Avatar className={classes.avatar}>{ <LockOutlinedIcon /> }</Avatar>
         <Typography component="h1" variant="h5">
           Sign up
         </Typography>
+        {currentUser.email}
+        {error && <Alert severity="error">{error}</Alert>}
         <form className={classes.form} noValidate onSubmit={handleSubmit}>
           <Grid container spacing={2}>
-            <Grid item xs={12} sm={6}>
+            {/* <Grid item xs={12} sm={6}>
               <TextField
                 autoComplete="fname"
                 name="firstName"
@@ -103,7 +115,7 @@ export default function Signup() {
                 ref={lNameRef}
                 autoComplete="lname"
               />
-            </Grid>
+            </Grid> */}
             <Grid item xs={12}>
               <TextField
                 variant="outlined"
@@ -112,7 +124,9 @@ export default function Signup() {
                 id="email"
                 label="Email Address"
                 name="email"
-                ref={emailRef}
+                type="text"
+                inputRef={emailRef}
+                onClick={() => emailRef.current.focus()}
                 autoComplete="email"
               />
             </Grid>
@@ -126,7 +140,20 @@ export default function Signup() {
                 type="password"
                 id="password"
                 autoComplete="current-password"
-                ref={passwordRef}
+                inputRef={passwordRef}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                variant="outlined"
+                required
+                fullWidth
+                name="cofirmpassword"
+                label="Confirm Password"
+                type="password"
+                id="cofirmpassword"
+                autoComplete="current-password"
+                inputRef={cofirmpasswordRef}
               />
             </Grid>
           </Grid>
@@ -142,7 +169,7 @@ export default function Signup() {
           </Button>
           <Grid container justify="flex-end">
             <Grid item>
-              <Link href="#" variant="body2">
+              <Link to="/login" variant="body2">
                 Already have an account? Sign in
               </Link>
             </Grid>
--- ezeebook/src/firebase.js
+++ ezeebook/src/firebase.js
@@ -2,24 +2,24 @@ import firebase from "firebase/app"
 import "firebase/auth"
 import "firebase/database"
 
-// const app = firebase.initializeApp({
-//   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-//   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-//   projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-//   storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-//   messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-//   appId: process.env.REACT_APP_FIREBASE_APP_ID,
-// });
-//Todo: get from env files
 const app = firebase.initializeApp({
-  apiKey: 'AIzaSyAxknPf-xiyxv7MqM-WR0xEAxihPl0yTRc',
-  authDomain: 'ezeebook-dev.firebaseapp.com',
-  projectId: 'ezeebook-dev',
-  storageBucket: 'ezeebook-dev.appspot.com',
-  messagingSenderId: '1040461298600',
-  appId: '1:1040461298600:web:80ccde9b218783b67bfb9d',
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 });
+// //Todo: get from env files
+// const app = firebase.initializeApp({
+//   apiKey: 'AIzaSyAxknPf-xiyxv7MqM-WR0xEAxihPl0yTRc',
+//   authDomain: 'ezeebook-dev.firebaseapp.com',
+//   projectId: 'ezeebook-dev',
+//   storageBucket: 'ezeebook-dev.appspot.com',
+//   messagingSenderId: '1040461298600',
+//   appId: '1:1040461298600:web:80ccde9b218783b67bfb9d',
+// });
 
-export const auth = app.auth()
+export const auth = app.auth();
 export default app.database();
 
