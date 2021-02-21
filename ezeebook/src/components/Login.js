5bca37e28ad279a9e2ac28dfc99223a9399c5f87
Kinjal Shah <kinjal_shah1311@yahoo.com>
2021-02-21 18:55:46 -0500

    added error message

--- ezeebook/src/components/Login.js
+++ ezeebook/src/components/Login.js
@@ -1,4 +1,4 @@
-import React, {useRef, useState} from "react";
+import React, { useRef, useState } from "react";
 import Avatar from "@material-ui/core/Avatar";
 import Button from "@material-ui/core/Button";
 import CssBaseline from "@material-ui/core/CssBaseline";
@@ -6,7 +6,7 @@ import TextField from "@material-ui/core/TextField";
 import FormControlLabel from "@material-ui/core/FormControlLabel";
 import Checkbox from "@material-ui/core/Checkbox";
 import Grid from "@material-ui/core/Grid";
-import Alert from '@material-ui/lab/Alert';
+import Alert from "@material-ui/lab/Alert";
 import Box from "@material-ui/core/Box";
 import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
 import Typography from "@material-ui/core/Typography";
@@ -62,12 +62,17 @@ export default function Login() {
 
   function handleSubmit(e) {
     e.preventDefault();
-    
+
     try {
       setError("");
       setLoading(true);
-      login(emailRef.current.value, passwordRef.current.value);
-      history.push("/");
+      login(emailRef.current.value, passwordRef.current.value)
+        .then((x) => {
+          history.push("/");
+        })
+        .catch((ex) => {
+          setError("Incorrect Email or Password. Please try again!");
+        });
     } catch {
       setError("Failed to log in");
     }
