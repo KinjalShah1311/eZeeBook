19dcb0dc95b244459c21b87fdec1687f7cf3c78f
anshulbaweja <anshul.baweja92@gmail.com>
2021-03-13 18:54:09 -0500

    API configuration
    clubbed two API's  together : one fetching the longitude and latitude , another one fetching the list of hotel names

--- src/api/DataService.js
+++ src/api/DataService.js
@@ -6,18 +6,13 @@ import axios from 'axios'
 class DataService{
 
 
-    retrieveAllData(name){
-
+  retrieveLocation(name){
+          console.log(name);
         const options = {
             method: 'GET',
-            url: 'https://hotels4.p.rapidapi.com/properties/get-details',
+            url: 'https://hotels4.p.rapidapi.com/locations/search',
             params: {
-              id: '424010',
-              locale: 'en_US',
-              currency: 'USD',
-              checkOut: '2020-01-15',
-              adults1: '1',
-              checkIn: '2020-01-08'
+              query: name, locale: 'en_US'
             },
             headers: {
               'x-rapidapi-key': 'aa3dfad0d3msh91e468cf37f9259p167861jsn51220deec226',
@@ -28,6 +23,27 @@ class DataService{
         return  axios.request(options);
 
 
+    }
+    retriveHotelNames(lat,lon){
+      const options = {
+        method: 'GET',
+        url: 'https://hotels-com-free.p.rapidapi.com/srle/listing/v1/brands/hotels.com',
+        params: {
+          lat: lat,
+          lon: lon,
+          checkIn: '2021-01-27',
+          checkOut: '2021-01-28',
+          rooms: '1',
+          locale: 'en_US',
+          currency: 'USD',
+          pageNumber: '1'
+        },
+        headers: {
+          'x-rapidapi-key': 'aa3dfad0d3msh91e468cf37f9259p167861jsn51220deec226',
+          'x-rapidapi-host': 'hotels-com-free.p.rapidapi.com'
+        }
+      };
+      return  axios.request(options);
     }
 
    
